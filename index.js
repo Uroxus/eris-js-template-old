a7ccd9e259340e230466f42785a23982df33826f
Uroxus <thepointlessemails@gmail.com>
2021-11-14 21:33:15 +0000

    Bot runner

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * @file Create and start a new instance of the bot
+ */
+
+import dotenv from "dotenv"
+dotenv.config()
+
+import botClient from "./Classes/Client.js"
+
+new botClient( ( process.env.TESTING === "true" ? process.env.DEV_TOKEN : process.env.PROD_TOKEN ), {
+    "defaultImageFormat": "png",
+    "restMode": true,
+    "maxShards": "auto",
+    "intents": [ "guilds", "guildMessages", "guildIntegrations", "directMessages" ]
+} )
