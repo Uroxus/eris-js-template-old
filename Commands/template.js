36b4d79400fe6f20ef8297e15e141a490e070be1
Uroxus <thepointlessemails@gmail.com>
2021-11-27 00:37:37 +0000

    Add example text based response

--- Commands/template.js
+++ Commands/template.js
@@ -3,12 +3,13 @@
  */
 
 import { Command } from "../Classes/Command.js"
-import { Constants } from "eris"
+import { Constants, Message } from "eris"
 
 export default class TemplateCommand extends Command {
     constructor() {
         super( {
             "name": "template",
+            "aliases": [ "tmplt" ],
             "applicationCommands": [ {
                 "name": "template",
                 "description": "Template for a slash command",
@@ -21,6 +22,31 @@ export default class TemplateCommand extends Command {
                 "name": "Template Message Action",
                 "type": Constants.ApplicationCommandTypes.MESSAGE
             } ]
+        } )
+    }
+
+    /**
+     * 
+     * @param {Message} Message 
+     */
+    textCommand ( Message ) {
+        Message.channel.createMessage( {
+            "embeds": [
+                {
+                    "author": {
+                        "name": Message.author.username,
+                        "icon_url": Message.author.dynamicAvatarURL()
+                    },
+                    "thumbnail": {
+                        "url": Message.author.dynamicAvatarURL()
+                    },
+                    "description": "Template text command response",
+                    "color": parseInt( process.env.EMBED_DEFAULT )
+                }
+            ],
+            "messageReference": {
+                "messageID": Message.id
+            }
         } )
     }
 }
