e90f62facad852551fcbebf3cd8ba7fcd5e506b1
Uroxus <thepointlessemails@gmail.com>
2021-11-29 22:12:36 +0000

    This doesn't work

--- Modules/Utilities.js
+++ Modules/Utilities.js
@@ -1,8 +1,6 @@
 /**
  * @file Export useful functions used throughout the project
  */
-import { promises as fs } from "fs"
-import path from "path"
 
 /**
  * Extract the ID & token from a webhook URL
@@ -13,13 +11,4 @@ import path from "path"
 export function extractWebhookElements ( webhook ) {
     const elements = webhook.split( "/" )
     return [ elements.at( -2 ), elements.at( -1 ) ]
-}
-
-/**
- * Get a list of filenames from a given directory 
- * @param {String} directory The path to the directory where the files are expected
- * @returns {Array<String>} Filenames of the things found in the directory
- */
-export function fetchFilesFromDirectory ( directory ) {
-    return ( await fs.readdir( fileURLToPath( `file://${ path.join( path.dirname( fileURLToPath( import.meta.url ) ), directory ) }` ) ) )
 }
