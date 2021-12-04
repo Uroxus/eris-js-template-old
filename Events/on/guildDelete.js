6711ec344dd2f13980ac29811822fa569ef2497e
Uroxus <thepointlessemails@gmail.com>
2021-12-04 20:51:09 +0000

    Skip logging unavailable guild deletion

--- Events/on/guildDelete.js
+++ Events/on/guildDelete.js
@@ -15,27 +15,29 @@ import { extractWebhookElements } from "../../Modules/Utilities.js"
  */
 export default async function ( Client, Guild ) {
     if ( process.env.GUILD_WEBHOOK && ( ( process.env.TESTING === "true" && process.env.GUILD_WEBHOOK_ON_TESTING === 'true' ) || process.env.TESTING !== "true" ) ) {
-        const [ id, token ] = extractWebhookElements( process.env.GUILD_WEBHOOK )
-        Client.executeWebhook( id, token, {
-            "embeds": [
-                {
-                    "title": "Guild Left",
-                    "description": `${ Guild.name ?? `*${ Guild.id }*` }`,
-                    "color": parseInt( process.env.EMBED_RED ),
-                    "timestamp": new Date(),
-                    "footer": { "text": `${ Client.getUserString( Client.user ) }`, "icon_url": Client.user.dynamicAvatarURL() },
-                    ...( Guild.memberCount ) && {
-                        "fields": [
-                            {
-                                "name": "Members",
-                                "value": Guild.memberCount
-                            }
-                        ]
+        if ( !Client.unavailableGuilds.has( Guild.id ) ) {
+            const [ id, token ] = extractWebhookElements( process.env.GUILD_WEBHOOK )
+            Client.executeWebhook( id, token, {
+                "embeds": [
+                    {
+                        "title": "Guild Left",
+                        "description": `${ Guild.name ?? `*${ Guild.id }*` }`,
+                        "color": parseInt( process.env.EMBED_RED ),
+                        "timestamp": new Date(),
+                        "footer": { "text": `${ Client.getUserString( Client.user ) }`, "icon_url": Client.user.dynamicAvatarURL() },
+                        ...( Guild.memberCount ) && {
+                            "fields": [
+                                {
+                                    "name": "Members",
+                                    "value": Guild.memberCount
+                                }
+                            ]
+                        }
                     }
-                }
-            ]
-        } ).catch( ( error ) => {
-            Logger.warn( `[GUILDS] Failed to post guild delete status: ${ error }` )
-        } )
+                ]
+            } ).catch( ( error ) => {
+                Logger.warn( `[GUILDS] Failed to post guild delete status: ${ error }` )
+            } )
+        }
     }
 }
