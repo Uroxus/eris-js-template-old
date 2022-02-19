f41c9544992ead2c1e5aa08441455161ac5c202c
Uroxus <thepointlessemails@gmail.com>
2022-02-19 23:26:19 +0000

    Winston logging module

---
+++ Modules/Logger.js
@@ -0,0 +1,43 @@
+import winston from "winston"
+import dotenv from "dotenv"
+dotenv.config()
+
+const jsonFormat = winston.format.combine( winston.format.timestamp( { format: 'HH:mm:ss DD-MM-YY' } ), winston.format.json() )
+
+/**
+ * Create a log of some important information
+ * @exports {Logger}
+ * @method {emerg}
+ * @method {alert} 
+ * @method {crit}
+ * @method {error} Something went wrong that shouldn't have
+ * @method {warning} Something went wrong but it's not catastrophic
+ * @method {notice} 
+ * @method {info} Logging a status or some useful information
+ */
+export const Logger = winston.createLogger( {
+    level: process.env.TESTING === "true" ? "debug" : "warning",
+    levels: winston.config.syslog.levels,
+
+    transports: [
+        new winston.transports.Console( {
+            format: winston.format.combine(
+                winston.format( ( info ) => ( { ...info, level: info.level.toUpperCase() } ) )(),
+                winston.format.errors( { stack: true } ),
+                winston.format.colorize(),
+                winston.format.timestamp( { format: 'HH:mm:ss DD-MM-YY' } ),
+                winston.format.align(),
+                winston.format.printf( info => `${ info.timestamp } [${ info.level }]: ${ info.message }` ) )
+        } ),
+        new winston.transports.File( { filename: "Logs/error.log", level: "error", format: jsonFormat } ),
+        new winston.transports.File( { filename: "Logs/all.log", maxSize: 500000000, format: jsonFormat } )
+    ],
+
+    exceptionHandlers: [
+        new winston.transports.File( { filename: "Logs/exceptions.log", maxsize: 20000000, format: jsonFormat } )
+    ],
+
+    rejectionHandlers: [
+        new winston.transports.File( { filename: "Logs/rejections.log", maxsize: 20000000, format: jsonFormat } )
+    ]
+} )
