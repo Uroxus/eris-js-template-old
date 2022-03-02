37eecb7be2e8d75983dffe487c3b6687a1dbbeb0
Uroxus <thepointlessemails@gmail.com>
2022-03-02 21:17:57 +0000

    Export colours as base 10 instead of hex

--- Constants/Message.js
+++ Constants/Message.js
@@ -1,6 +1,6 @@
 export const EmbedColours = {
-    "green": 0x43B581,
-    "orange": 0xF09E47,
-    "red": 0xF04947,
-    "default": 0xFFFFFF
+    "green": parseInt( 0x43B581 ),
+    "orange": parseInt( 0xF09E47 ),
+    "red": parseInt( 0xF04947 ),
+    "default": parseInt( 0xFFFFFF )
 }
