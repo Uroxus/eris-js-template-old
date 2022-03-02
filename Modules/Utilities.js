f0212754404b08ca021eeed099c47e2e570aace5
Uroxus <thepointlessemails@gmail.com>
2022-03-02 21:16:23 +0000

    Add getUserString method

--- Modules/Utilities.js
+++ Modules/Utilities.js
@@ -2,6 +2,8 @@
  * @file Exports useful functions to use throughout the project
  */
 
+import { User } from "eris"
+
 /**
  * From a Discord API webhook link, extract the ID and token
  * @param {String} webhook Webhook URL string
@@ -11,4 +13,14 @@
 export function extractWebhookValues ( webhook ) {
     const values = webhook.split( "/" )
     return [ values.at( -2 ), values.at( -1 ) ]
+}
+
+/**
+ * Get the username#discriminator string of a user
+ * @param {User} User
+ * @returns {String} name#discrim
+ * @example Uroxoos#0420
+ */
+export function getUserString ( User ) {
+    return `${ User.username }#${ User.discriminator }`
 }
