65b8b4a66764613fcb5d80fa9be399ac0752c754
Uroxus <thepointlessemails@gmail.com>
2022-03-26 20:35:47 +0000

    improve client options

--- index.js
+++ index.js
@@ -7,24 +7,85 @@ import { Fleet } from "eris-fleet"
 import dotenv from "dotenv"
 dotenv.config()
 
-import { BotWorker } from "./bot.js"
+import { BotWorker as Shard } from "./bot.js"
 import { inspect } from "util"
 
 const Admiral = new Fleet( {
-    BotWorker,
-    token: process.env.BOT_TOKEN,
+    BotWorker: Shard,
+    token: `Bot ${ process.env.BOT_TOKEN }`,
+
+    startingStatus: {
+        "status": "idle",
+        "game": {
+            "type": `Booting up...`
+        }
+    },
+
+    whatToLog: {
+        blacklist: [ "stats_update" ]
+    },
+
     clientOptions: {
         "defaultImageFormat": "png",
         "restMode": true,
-        "intents": [ "guilds", "guildMessages", "guildIntegrations", "directMessages" ]
+        "disableEvents": {
+            "GUILD_BAN_ADD": true,
+            "GUILD_BAN_REMOVE": true,
+            "GUILD_MEMBER_ADD": true,
+            "GUILD_MEMBER_REMOVE": true,
+            "GUILD_MEMBER_UPDATE": true,
+            "GUILD_ROLE_CREATE": true,
+            "GUILD_ROLE_DELETE": true,
+            "GUILD_ROLE_UPDATE": true,
+
+            "CHANNEL_CREATE": true,
+            "CHANNEL_DELETE": true,
+            "CHANNEL_UPDATE": true,
+
+            "MESSAGE_DELETE": true,
+            "MESSAGE_DELETE_BULK": true,
+            "MESSAGE_UPDATE": true,
+
+            "STAGE_INSTANCE_CREATE": true,
+            "STAGE_INSTANCE_DELETE": true,
+            "STAGE_INSTANCE_UPDATE": true,
+
+            "MESSAGE_REACTION_ADD": true,
+            "MESSAGE_REACTION_REMOVE": true,
+            "MESSAGE_REACTION_REMOVE_ALL": true,
+            "MESSAGE_REACTION_REMOVE_EMOJI": true,
+
+            "INVITE_CREATE": true,
+            "INVITE_DELETE": true,
+
+            "THREAD_UPDATE": true,
+            "THREAD_DELETE": true,
+
+            "PRESENCE_UPDATE": true,
+            "TYPING_START": true,
+            "USER_UPDATE": true,
+            "WEBHOOKS_UPDATE": true,
+            "VOICE_STATE_UPDATE": true
+        },
+        "allowedMentions": {
+            "everyone": false,
+            "roles": false,
+            "repliedUser": true,
+            "users": true
+        },
+        "intents": [ "guilds", "guildMessages", "guildIntegrations", "directMessages" ],
+        "messageLimit": 0,
     },
+
     "useCentralRequestHandler": true,
-    "whatToLog": {
-        "whitelist": [ "all_clusters_launched", "all_services_launched", "cluster_launch", "service_launch" ]
-    }
 } )
 
 if ( isMaster ) {
+    const logger = winston.createLogger( {
+
+    } )
+
+
     Admiral.on( 'log', msg => console.log( msg ) )
     Admiral.on( 'debug', msg => console.debug( msg ) )
     Admiral.on( 'warn', msg => console.warn( msg ) )
